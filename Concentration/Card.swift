7582cf31bdb72a847eeca0ef3d6854b2edb39a59
michael2k <kms@kimmyungsun.com>
2018-09-24 15:08:30 +0900

    Initial Commit

---
+++ Concentration/Card.swift
@@ -0,0 +1,16 @@
+//
+//  Card.swift
+//  Concentration
+//
+//  Created by Kim Myungsun on 2018. 9. 24..
+//  Copyright © 2018년 com.kimmyungsun. All rights reserved.
+//
+
+import Foundation
+
+struct Card
+{
+    var isFaceUp = false
+    var isMatched = false
+    var identifier: Int
+}
---
+++ Concentration/Concentration.swift
@@ -0,0 +1,18 @@
+//
+//  Concentration.swift
+//  Concentration
+//
+//  Created by Kim Myungsun on 2018. 9. 24..
+//  Copyright © 2018년 com.kimmyungsun. All rights reserved.
+//
+
+import Foundation
+
+class Concentration
+{
+    var cards: Array<Card>
+    
+    func chooseCard(at index: Int) {
+        
+    }
+}

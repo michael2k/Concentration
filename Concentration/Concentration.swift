3233e23e6eaea41d1967f7841961d68079723d7e
michael2k <kms@kimmyungsun.com>
2018-09-25 10:26:40 +0900

    applied access controls and assertions..

--- Concentration/Concentration.swift
+++ Concentration/Concentration.swift
@@ -10,9 +10,9 @@ import Foundation
 
 class Concentration
 {
-    var cards = [Card]()
+    private(set) var cards = [Card]()
     
-    var indexOfOnAndOnlyFaceUpCard: Int? {
+    private var indexOfOnAndOnlyFaceUpCard: Int? {
         get {
             var foundIndex: Int?
             for index in cards.indices {
@@ -34,6 +34,7 @@ class Concentration
     }
     
     func chooseCard(at index: Int) {
+        assert(cards.indices.contains(index), "Concentration.chooseCard(at: \(index)): chosen index not in the cards")
         if !cards[index].isMatched {
             if let matchIndex = indexOfOnAndOnlyFaceUpCard, matchIndex != index {
                 // check if cards match
@@ -49,6 +50,7 @@ class Concentration
     }
     
     init(numberOfPairsOfCards: Int) {
+        assert(numberOfPairsOfCards > 0, "Concentration.init(\(numberOfPairsOfCards)): you must have at least on pair of cards")
         for _ in 1...numberOfPairsOfCards {
             let card = Card()
             cards += [card, card]
--- Concentration/ViewController.swift
+++ Concentration/ViewController.swift
@@ -10,22 +10,23 @@ import UIKit
 
 class ViewController: UIViewController
 {
-    lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards )
+    private lazy var game: Concentration = Concentration(numberOfPairsOfCards: numberOfPairsOfCards )
     
     var numberOfPairsOfCards : Int {
         return ( cardButtons.count + 1 ) / 2
     }
-    var flipCount = 0 {
+    
+    private(set) var flipCount = 0 {
         didSet {
             flipCountLabel.text = "Flips: \(flipCount)"
        }
     }
 
-    @IBOutlet weak var flipCountLabel: UILabel!
+    @IBOutlet private weak var flipCountLabel: UILabel!
     
-    @IBOutlet var cardButtons: [UIButton]!
+    @IBOutlet private var cardButtons: [UIButton]!
     
-    @IBAction func touchCard(_ sender: UIButton) {
+    @IBAction private func touchCard(_ sender: UIButton) {
         flipCount += 1
         if let cardNumber = cardButtons.index(of: sender) {
             game.chooseCard(at: cardNumber)
@@ -35,7 +36,7 @@ class ViewController: UIViewController
         }
     }
     
-    func updateViewFromModel() {
+    private func updateViewFromModel() {
         for index in cardButtons.indices {
             let button = cardButtons[index]
             let card = game.cards[index]
@@ -49,11 +50,11 @@ class ViewController: UIViewController
         }
     }
     
-    var emojiChoices = ["🎃","👻","🦇","😱","🙀","👿","🍭","🍎","🍬"]
+    private var emojiChoices = ["🎃","👻","🦇","😱","🙀","👿","🍭","🍎","🍬"]
     
-    var emoji = [Int:String]()
+    private var emoji = [Int:String]()
     
-    func emoji(for card: Card) -> String {
+    private func emoji(for card: Card) -> String {
         if emoji[card.identifier] == nil, emojiChoices.count > 0 {
             let randomIndex = Int(arc4random_uniform(UInt32(emojiChoices.count)))
             emoji[card.identifier] = emojiChoices.remove(at: randomIndex)
